884e9cfeb46d75ccf52209c60377d72ad527c5a2
노정현 <30489028+MarkNoh@users.noreply.github.com>
2022-01-18 15:59:56 +0900

    Add files via upload
    Initial Commit (gradle)

---
+++ src/main/java/com/bluewhite/bluewhite/BluewhiteApplication.java
@@ -0,0 +1,13 @@
+package com.bluewhite.bluewhite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluewhiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluewhiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluewhite/bluewhite/BluewhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluewhite.bluewhite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluewhiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
